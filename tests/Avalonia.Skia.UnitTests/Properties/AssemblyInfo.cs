469a3538c2cc19fb3f566ad8017605b6764d256b
Nelson Carrillo <ncarrillo1@outlook.com>
2018-05-29 21:16:14 -0400

    WIPD

--- tests/Avalonia.Skia.UnitTests/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) The Avalonia Project. All rights reserved.
-// Licensed under the MIT license. See licence.md file in the project root for full license information.
-
-using System.Reflection;
-using Xunit;
-
-// Don't run tests in parallel.
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
